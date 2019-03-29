db1e1ef79d8bdcdf8d6e9958713052236bc4123c
Evan Hadfield <hadfield.evan@gmail.com>
2019-03-29 13:29:28 -0700

    add reload button, and additional content.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
   $("form#whatlanguage").submit(function(event){
     var question1 = $("select#question1").val();
@@ -6,8 +8,32 @@ $(document).ready(function(){
     var question4 = $("select#question4").val();
     var question5 = $("select#question5").val();
 
-    if(question1 === "2"){
+    if(question5 === "15"){
+      $("#other").show();
+      $("#javascript").hide();
+      $("#python").hide();
+      $("#ruby").hide();
+      $("#php").hide();
+      $("#whatlanguage").hide();
+    }else if(question1 === "2" && question3 === "8"){
       $("#javascript").show();
+      $("#csharp").show();
+      $("#whatlanguage").hide();
+    }else if(question1 === "2"){
+      $("#javascript").show();
+      $("#whatlanguage").hide();
+    }else if(question2 === "4"){
+      $("#php").show();
+      $("#whatlanguage").hide();
+    }else if(question3 === "7"){
+      $("#ruby").show();
+      $("#whatlanguage").hide();
+    }else if(question4 === "11"){
+      $("#python").show();
+      $("#whatlanguage").hide();
+    }else if(question3 === "8"){
+      $("#csharp").show();
+      $("#whatlanguage").hide();
     }
     event.preventDefault();
   });
