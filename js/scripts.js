048c393c5cd011beabc03b604fa91fd7de11c4f1
Evan Hadfield <hadfield.evan@gmail.com>
2019-03-29 11:40:44 -0700

    add content and initial css styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-$(function() {
+$(document).ready(function()) {
   $("form#whatlanguage").submit(function() {
     var question1 = $("select#question1").val();
-    
+    var question2 = $("select#question2").val();
+    var question3 = $("select#question3").val();
+    var question4 = $("select#question4").val();
+    var question5 = $("select#question5").val();
   }
 }
